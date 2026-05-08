d39265d90739dfac5a5b9650d2b526ead0bc6fe3
jck <jckleinbourg@gmail.com>
2026-05-08 19:16:05 +0200

    Prepare for GitHub Page

---
+++ index.js
@@ -0,0 +1,8 @@
+const GAME_LIST = [
+  ["burger-runner", "#018750"],
+  ["jimmy-coin", "#7e2553"],
+  ["arkadroid", "#1b2853"],
+  ["bensquest-alpha", "#018750"],
+  ["sharks-n-worms", "#1b2853"],
+  ["burger-biker", "#ab5236"]
+];
